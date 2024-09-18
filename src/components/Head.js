84adc2d420652424d0d487248587830bdf575d34
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-09-18 20:54:26 +0530

    update

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,17 @@
 import { useDispatch } from "react-redux";
-import { HAMBURGERICON, USERICON, YOUTUBELOGO, YOUTUBESEARCHSUGGESTIONSAPI } from "../utils/contacts";
+import {
+  HAMBURGERICON,
+  USERICON,
+  YOUTUBELOGO,
+  YOUTUBESEARCHSUGGESTIONSAPI,
+} from "../utils/contacts";
 import { toggleMenu } from "../utils/appSlice";
 import { useEffect, useState } from "react";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setsuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     console.log(searchQuery);
@@ -12,15 +19,14 @@ const Head = () => {
 
     return () => {
       clearTimeout(timer);
-    }
+    };
   }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
     const data = await fetch(YOUTUBESEARCHSUGGESTIONSAPI + searchQuery);
     const json = await data.json();
-
-    console.log(json[1])
-  }
+    setsuggestions(json[1]);
+  };
 
   const dispatch = useDispatch();
 
@@ -28,9 +34,8 @@ const Head = () => {
     dispatch(toggleMenu());
   };
 
-
   return (
-    <div className="w-screen flex p-4 m-1 shadow-lg justify-between ">
+    <div className="flex p-4 m-1 justify-between ">
       <div className="flex  cursor-pointer gap-3 ">
         <img
           onClick={() => handletoggleMenu()}
@@ -43,16 +48,34 @@ const Head = () => {
         </a>
       </div>
       <div className="cursor-pointer ">
-        <input
-          className="w-[450px] px-2 border-2 py-1.5 rounded-l-full "
-          type="text"
-          placeholder="Search"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="px-2 py-1.5 border-2 rounded-r-full bg-gray-300 hover:bg-opacity-80">
-          Search
-        </button>
+        <div>
+          <input
+            className="w-[450px] px-4 border-2 py-1.5 rounded-l-full "
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="px-2 py-1.5 border-2 rounded-r-full bg-gray-300 hover:bg-opacity-80">
+            Search
+          </button>
+          {showSuggestions && (
+            <div className="fixed pt-2 bg-white w-[28rem] pl-4 shadow-lg rounded-lg">
+              <ul>
+                {suggestions.map((suggestion) => (
+                  <li
+                    key={suggestion}
+                    className="py-1 hover:bg-gray-100 hover:rounded-lg"
+                  >
+                    {suggestion}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
       <div className=" flex cursor-pointer">
         <img className="h-7" alt="UserIcon" src={USERICON} />
